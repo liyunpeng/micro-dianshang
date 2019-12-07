69beb9f1eac00aee0d37070a430a8e9e7ef706f7
lijinyang <kylin27@126.com>
2019-12-07 08:18:19 +0800

    使gorm连接数据库可用

--- database.go
+++ database.go
@@ -1,19 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func CreateConnection() (*gorm.DB, error) {
-	host := "192.168.0.145"
-	user := "user1"
-	dbName := "shopping"
-	password := "123456"
-	return gorm.Open("mysql", fmt.Sprintf(
-		"%s:%s@tcp(%s:3306)/%s?charset=utf8&parseTime=True&loc=Local",
-		user, password, host, dbName,
-	),
-	)
+	//host := "192.168.0.145"
+	//user := "user1"
+	//dbName := "shopping"
+	//password := "123456"
+	//return gorm.Open("mysql", fmt.Sprintf(
+	//	"%s:%s@tcp(%s:3306)/%s?charset=utf8&parseTime=True&loc=Local",
+	//	user, password, host, dbName,
+	//	),
+	//)
+	db, err := gorm.Open(
+		"mysql", "root:123456@/shopping?charset=utf8&parseTime=True&loc=Local")
+
+	return  db, err
+
+
 }
